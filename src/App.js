424f54ed0975da6e0873009635979ed089c02c73
vivek <viveknetha12@gmail.com>
2024-12-05 10:58:12 +0530

    added heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> this is edited by vivek </h1>
+      <h1>MERN2409</h1>
+      <h2> this is edited by vivek </h2>
     </div>
   );
 }
